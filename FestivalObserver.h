99767420f49a59d74318208d9c64f893aba9c6bb
u25622103-droid <u25622103@tuks.co.za>
2026-08-31 20:24:17 +0200

    Observer for the festival

---
+++ FestivalObserver.h
@@ -0,0 +1,59 @@
+#ifndef FESTIVALOBSERVER_H
+#define FESTIVALOBSERVER_H
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+class Notice {
+    private:
+        std::string type;
+        std::string description;
+
+    public:
+        enum NoticeType {
+            OPEN, 
+            CLOSE, 
+            SCHEDULE_CHANGE, 
+            CAPACITY_ALERT,
+            WEATHER_ALERT,
+            PAUSE,
+            RESUME,
+            EVACUATE
+        };
+
+        Notice(std::string, std::string);
+        std::string getNotice();
+};
+
+class FestivalSubject {
+    private:
+        std::vector<FestivalObserver*> observerList;
+
+    public:
+        FestivalSubject();
+        ~FestivalSubject();
+        void attach(FestivalObserver* observer);
+        void detach(FestivalObserver* observer);
+        void notify();
+};
+
+class FestivalControl : public FestivalSubject {
+    private:
+        Notice& currentNotice;
+
+    public:
+        FestivalControl();
+        void issueNotice(Notice notice);
+        void attach(FestivalObserver* observer);
+        void detach(FestivalObserver* observer);
+        void notify();
+};
+
+class FestivalObserver {
+    public:
+        virtual ~FestivalObserver() = default;
+        virtual void update(const Notice& notice) = 0;
+};
+
+#endif //FESTIVALOBSERVER_H
